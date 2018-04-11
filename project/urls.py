b42f6fe4a2a9679786c143795f46dcbd109dbc56
Rogers de Pelle <rogersdepelle@gmail.com>
2018-04-11 09:03:45 -0300

    Adiciona icones aosmapas

--- project/urls.py
+++ project/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
-from maps.views import HomePageView
+from maps.views import ParticipatoryMapsListView, ParticipatoryMapDetailView
+
 
 urlpatterns = [
-    path('', HomePageView.as_view()),
+    path('', ParticipatoryMapsListView.as_view(), name='pmap_list_view'),
+    path('mapa/<int:pk>/', ParticipatoryMapDetailView.as_view(), name='pmap_detail_view'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
